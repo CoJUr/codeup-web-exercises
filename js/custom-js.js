4fc7bfd65f589dfd4abbdb1f6f55ab6156d654bd
“Cory <coryurton@gmail.com>
2022-05-13 12:27:12 -0500

    forgot to commit after each of the changes, oops

--- js/custom-js.js
+++ js/custom-js.js
@@ -2,6 +2,35 @@
 
 
 $(document).ready(function() {
-    alert("ready")
+    //both lines accomplish the same thing, line 6 with jquery
+    $('pageLink').html(value:"<h1>Link to somewhere</h1>");
+
+//vanilla JS
+// document.getElementById("pageLink").innerHTML = "<h1>link to somewhere</h1>"
+
+    //class selector
+    //returns an array of elements
+    console.log($('.list-item'));
+
+    //not manipulating a jquery object at this point
+    $('.list-item')[0].innerHTML = 'Hello'
+
+
+    //element selector
+    $('main').css('background-color', 'cadetblue')
+
+    //want to change more than 1 property, then pass an object with key-value pairs
+    $('li').css( {
+        'background-color': 'hotpink',
+        'border-bottom': '1px solid black',
+    })
+
+
+    //selecting multiple elements. returns an array of element nodes
+    $('h1, #pageLink, #list').css('color', 'forestgreen')
+
+    //all selector example. select all elements and give borders.
+    $('*').css('border', '2px dotted black')
 
 });
+
