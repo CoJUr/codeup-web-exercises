148c29957ea4a6470c76bedf44b5a0d7fce325fe
“Cory <coryurton@gmail.com>
2022-05-20 16:24:14 -0500

    feat: daily forecast

--- js/weather-map.js
+++ js/weather-map.js
@@ -1,28 +1,41 @@
-$.get("http://api.openweathermap.org/data/2.5/weather", {
+$.get("http://api.openweathermap.org/data/2.5/onecall", {
     APPID: OPEN_WEATHER_APPID,
-    q:     "Austin, TX US",
+    lat:    30.266666,
+    lon:   -97.733330,
     units: "imperial"
 }).done(function(data) {
-    console.log(data);
-});
+    console.log('The entire response:', data);
+    console.log(data.daily[0].temp.day);
+    $('#data').append("<h1>" + "Current temperature in Austin" + "</h1>"
+        + "<p>" + data.current.temp + "</p>");
+    for (let i = 0; i < 5; i++) {
+            $('#data').append("<h2>" + " 5-Day Projected forecast: " + "</h2>" +
+                "<p>" + "Average daily temperate: " + data.daily[i].temp.day + "</p>" +
+            "<h6>" + "Expected high: " + data.daily[i].temp.max + " "
+                + "Expected low: " + data.daily[i].temp.min + "</h6>")
+        }
+    })
+
+
+
 
 // $.get("http://api.openweathermap.org/data/2.5/onecall", {
 //     APPID: OPEN_WEATHER_APPID,
-//     lat:    29.423017,
-//     lon:   -98.48527,
+//     lat:    30.266666,
+//     lon:   -97.733330,
 //     units: "imperial"
 // }).done(function(data) {
 //     console.log('The entire response:', data);
 //     console.log('Diving in - here is current information: ', data.current);
 //     console.log(data.current.temp) // prints 83.46
-//     console.log(data.current.weather[0].description) //weather is an array. should print the word haze
-//     console.log(data.daily);
-//     data.daily.forEach(function(dailyForecast, index) {
-//         if (index < 5) {
-//             console.log(dailyForecast.temp.max) //prints an array with 8 items
-//             console.log(dailyForecast.temp.min) //prints another 8 item array
-//         }
-//
-//     })
-    // console.log('A step further - information for tomorrow: ', data.daily[1]);
+//     // console.log(data.current.weather[0].description) //weather is an array. should print the word haze
+//     // console.log(data.daily);
+//     // data.daily.forEach(function(dailyForecast, index) {
+//     //     if (index < 5) {
+//     //         console.log(dailyForecast.temp.max) //prints an array with 8 items
+//     //         console.log(dailyForecast.temp.min) //prints another 8 item array
+//     //     }
+//     //
+//     // })
+//     // console.log('A step further - information for tomorrow: ', data.daily[1]);
 // });
