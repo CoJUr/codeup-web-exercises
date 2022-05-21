f9da7dfd619d32219f504b7ecae832bd2eed9508
“Cory <coryurton@gmail.com>
2022-05-21 00:26:02 -0500

    began new project

--- js/app2.js
+++ js/app2.js
@@ -1,12 +1,63 @@
-const inputValue = document.querySelector('.weatherInput')
-const button = document.querySelector(".submit")
+const app = {
+    init: () => {
+        document
+            .getElementById('btnGet')
+            .addEventListener('click', app.fetchWeather);
+        document
+            .getElementById('btnCurrent')
+            .addEventListener('click', app.getLocation)
+    },
+    fetchWeather: (ev) => {
+        //lat and long values for fetching weather
+        let lat = document.getElementById('latitude').value;
+        let lon = document.getElementById('longitude').value;
+        let key = 'ef0a1e043dbcad043f8230e14b0d82aa'
+        let lang = 'en';
+        let units = 'imperial'
+        let url = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}&units=${units}&lang=${lang}`
+        //fetch the weather
+        fetch(url)
+            .then((resp) => {
+                if (!resp.ok) throw new Error(resp.statusText);
+                return resp.json();
+            })
+            .then((data) => {
+                app.showWeather(data);
+            })
+            .catch(console.err)
+    },
+    getLocation: (ev) => {
+        let opts = {
+            enableHighAccuracy: true,
+            timeout: 1000 * 10, //10s
+            maximumAge : 1000 * 60 * 5, // 5min
+        };
+        navigator.geolocation.getCurrentPosition(app.ftw, app.wtf, opts);
+    },
+    ftw: (position) => {
+        //got position
+        document.getElementById('latitude').value =
+            position.coords.latitude.toFixed(2);
+        document.getElementById('longitude').value =
+            position.coords.longitude.toFixed(2)
+    },
+    wtf: (err) => {
+        //geolocation failed
+        console.error(err);
+    },
+    showWeather: (resp) => {
+        console.log(resp);
+        let row = document.querySelector('.weather.row');
+        //clear out the old weather and add the new
+        // row.innerHTML = "";
+        row.innerHTML = resp.daily
+    }
+}
 
-button.addEventListener('click', function() {
 
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=6286f997e626d4af0208815eae8aed27')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-        })
-})
 
+
+
+
+
+app.init();
