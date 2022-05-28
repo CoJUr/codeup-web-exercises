5060bad1dd2ddbb1b61f87f3ad0123610f114269
“Cory <coryurton@gmail.com>
2022-05-27 22:28:54 -0500

    V almost done

--- js/V.js
+++ js/V.js
@@ -1,17 +1,31 @@
 let weather = {
     apiKey: 'b928fc223761c64dc4bda9049f2acf',
-    fetchWeather: function () {
+    fetchWeather: function (city) {
         fetch('https://api.openweathermap.org/data/2.5/weather?q='
             + city
-            +'&units=metric&appid='
-            + appKey
+            + '&units=metric&appid='
+            + this.apiKey
         ).then((response) => response.json())
-            .then((data) => console.log(data))
+            .then((data) => this.displayWeather(data));
     },
-    displayWeather
-
-
-
-
-
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind
+        console.log(name,icon,description,temp,humidity,speed)
+        document.querySelector('.city').innerText = `Weather in ${name}`
+        document.querySelector('.icon').src = 'https://openweathermap.org/img/wn/'+icon+'@2x.png'
+        document.querySelector('.description').innerText = description
+        document.querySelector('.temp').innerText = `${temp}°`
+        document.querySelector('.humidity').innerText = `Humidity: ${humidity}%`
+        document.querySelector('.wind').innerText = `Wind speed: ${speed} km/h`
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector('.search-bar').value)
+    }
 }
+
+document.querySelector('.search button').addEventListener('click', function() {
+    weather.search();
+});
