330970d0e49862ea9b4f55d4130a82e25bc0f677
“Cory <coryurton@gmail.com>
2022-05-27 20:12:47 -0500

    hiding key

--- js/V.js
+++ js/V.js
@@ -1,5 +1,5 @@
 let weather = {
-    apiKey: 'b928fc223761c64dc4bda9049f2acfc', //missing 1
+    apiKey: 'b928fc223761c64dc4bda9049f2acf',
     fetchWeather: function () {
         fetch('https://api.openweathermap.org/data/2.5/weather?q='
             + city
