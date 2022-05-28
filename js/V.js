b4ec3c57d6fe2a6bf7959bb3f113015385a8ff6a
“Cory <coryurton@gmail.com>
2022-05-27 19:53:20 -0500

    V css complete

---
+++ js/V.js
@@ -0,0 +1,17 @@
+let weather = {
+    apiKey: 'b928fc223761c64dc4bda9049f2acfc', //missing 1
+    fetchWeather: function () {
+        fetch('https://api.openweathermap.org/data/2.5/weather?q='
+            + city
+            +'&units=metric&appid='
+            + appKey
+        ).then((response) => response.json())
+            .then((data) => console.log(data))
+    },
+    displayWeather
+
+
+
+
+
+}
