4a6bf66640fd532d25e52d4f9be5dd0d7b892b13
“Cory <coryurton@gmail.com>
2022-07-08 21:48:59 -0500

    it will make sense soon

---
+++ PyTips250/UnicodeMatchingStr.pyi
@@ -0,0 +1,12 @@
+# case folding is similar to lowercasing but more aggressive
+>>> a, b = "der Fluß", "der Fluss"
+
+>>> a.lower() == b.lower()
+False
+
+# casefold converts 'ß' "ss" which makes the 2 strings identical ß
+>>> a.casefold() == b.casefold()
+True
+
+>>> 'ß'.casefold()
+'ss'
