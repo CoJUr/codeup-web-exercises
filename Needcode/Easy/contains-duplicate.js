1f927dd74048be4de895956aec7bd827fec59f3f
“Cory <coryurton@gmail.com>
2022-08-25 22:25:18 -0500

    going over contains-duplicate some more

--- Needcode/Easy/contains-duplicate.js
+++ Needcode/Easy/contains-duplicate.js
@@ -55,3 +55,32 @@ containsDuplicate = function (nums) {
     }
     return false; //if the loop completes, return false because no duplicates were found.
 }
+
+
+//Method using Set() (exit early if true)
+containsDuplicate = function (nums) {
+    const numsSet = new Set();
+    for (const i of nums) {
+        if (numsSet.has(i)) {
+            return true;
+        }
+        numsSet.add(i);
+    }
+    return false;
+};
+
+
+//3rd method using Map() (Has to map entire array but code is more readable)
+containsDuplicate = function (nums) {
+    //create a new hashmap with all the items in the array. Any duplicates will be removed.
+    const totalWithoutDuplicates = new Map(nums.map((i) => [i]));
+
+    //check if the size of the initial array is larger than the new hashmap.
+    return totalWithoutDuplicates.size !== nums.length;
+};
+
+//Third method using Set() (Fastest runtime at 91.95% and very readable code)
+containsDuplicate = function (nums) {
+    //Pass the array into a Set() (which removes duplicates) and then compare its size to the original array.
+    return new Set(nums).size !== nums.length;
+};
