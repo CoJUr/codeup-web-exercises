144f3aea77a8d7b9411e1b51abc5f9ce8bf8017c
“Cory <coryurton@gmail.com>
2022-08-26 18:38:25 -0500

    anagram validation javascript style

--- Needcode/Easy/valid-anagram.js
+++ Needcode/Easy/valid-anagram.js
@@ -30,3 +30,19 @@ let isAnagram = function (s, t) {
     }
     return true; //if the loop completes, return true because the counts of the letters in the 2 strings are the same.
 }
+
+isAnagram = function(s, t) {
+    if (s.length !== t.length) return false;
+    const map = {};
+
+    for (let i = 0; i < s.length; i++) {
+        map[s[i]] ? map[s[i]]++ : map[s[i]] = 1;
+    }
+
+    for (let i = 0; i < t.length; i++) {
+        if (map[t[i]]) map[t[i]]--;
+        else return false;
+    }
+
+    return true;
+};
